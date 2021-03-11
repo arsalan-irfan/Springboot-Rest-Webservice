d60d4e8c85ec17077aca9e883b18ab0161e9f912
Muhammad Arsalan <43111371+arsalan318@users.noreply.github.com>
2021-03-11 16:30:17 +0500

    Done with Actuator, Api Versioning and Api Security

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/versioning/PersonV2.java
@@ -0,0 +1,23 @@
+package com.in28minutes.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV2 {
+    private Name name;
+
+    public PersonV2() {
+    }
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+
+
+
+}
